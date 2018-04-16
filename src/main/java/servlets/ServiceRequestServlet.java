2b745bc7aff9c3003873369c142ef751d84a4f88
Paweł Wądolny <viperowski@gmail.com>
2018-04-16 23:17:17 +0200

    Added base structure, modified navigation

---
+++ src/main/java/servlets/ServiceRequestServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/user/service-request")
+public class ServiceRequestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getRequestDispatcher("/WEB-INF/views/service-request.jsp").forward(request, response);
+	}
+
+
+
+}
